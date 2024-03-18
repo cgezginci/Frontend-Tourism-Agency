f8d329a7dc689ef676cba2a3b30b8fb73f946d1d
gorbadil <gorbadil@gmail>
2024-03-18 15:38:45 +0300

    first table and dataRoom added

--- Tourism Agency/src/Pages/Room/Room.jsx
+++ Tourism Agency/src/Pages/Room/Room.jsx
@@ -3,13 +3,53 @@ import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getRooms, addRoom, deleteRoom } from "../../API/Room";
 import { getRoomTypes } from "../../API/RoomTypes";
 import { getRoomFeatures } from "../../API/RoomFeatures";
+import { ClickAwayListener } from "@mui/base/ClickAwayListener";
 import { getHotels } from "../../API/Hotel";
 import "./Room.css";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import TextField from "@mui/material/TextField";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { Tab } from "@mui/material";
 
 function Room() {
+  const dataRoom = [
+    {
+      siteName: "Room Type",
+      dbName: "roomType.name",
+      type: "select",
+      options: "roomTypeQuery",
+      selectText: "Select a room type",
+    },
+    { siteName: "Stock", dbName: "stock", type: "number" },
+    { siteName: "Square Meter", dbName: "squareMeter", type: "number" },
+    { siteName: "Person Type", dbName: "personType", type: "text" },
+    { siteName: "Bed Number", dbName: "bedNumber", type: "number" },
+    { siteName: "Period Start", dbName: "periodStart", type: "date" },
+    { siteName: "Period End", dbName: "periodEnd", type: "date" },
+    { siteName: "Price", dbName: "price", type: "number" },
+    {
+      siteName: "Room Features",
+      dbName: "roomFeatures.name",
+      type: "checkbox",
+      options: "roomFeatureQuery",
+    },
+    {
+      siteName: "Hotel",
+      dbName: "hotelName",
+      type: "select",
+      selectText: "Select a hotel",
+      options: "hotelQuery",
+    },
+  ];
   const queryClient = useQueryClient();
+  const [updateRoom, setUpdateRoom] = useState({});
   const [newRoom, setNewRoom] = useState({
     roomType: {},
     stock: 0,
@@ -99,6 +139,19 @@ function Room() {
     });
   };
 
+  const handleUpdateBtn = (id) => {
+    console.log(id);
+    const newObj = roomQuery.data?.data?.data.find(
+      (room) => room.id === parseInt(id)
+    );
+    const hotelId = hotelQuery.data?.data?.data.find(
+      (hotel) => hotel.name === newObj.hotelName
+    );
+    newObj.hotel = { id: hotelId.id };
+    console.log(newObj);
+    setUpdateRoom({ ...newObj });
+  };
+
   return (
     <div className="room-container">
       <div className="add-room-container">
@@ -177,6 +230,9 @@ function Room() {
               <div key={roomFeature.id}>
                 <input
                   type="checkbox"
+                  checked={newRoom.roomFeatures.find(
+                    (roomF) => roomF.id === roomFeature.id
+                  )}
                   name="facilities"
                   value={roomFeature.id}
                   onChange={handleAddRoomFeature}
@@ -188,7 +244,7 @@ function Room() {
           <select
             name="hotel"
             required
-            value={newRoom.hotel.id || ""}
+            value={newRoom.hotel?.id || ""}
             onChange={(e) =>
               setNewRoom({
                 ...newRoom,
@@ -209,35 +265,130 @@ function Room() {
         </div>
       </div>
       <div className="rooms">
-        {roomQuery.data?.data?.data.map((room) => (
-          <div key={room.id} className="room">
-            <div className="room-info">
-              <span>Room Type: {room.roomType.name}</span>
-              <span>Stock: {room.stock}</span>
-              <span>Square Meter: {room.squareMeter}</span>
-              <span>Person Type: {room.personType}</span>
-              <span>Bed Number: {room.bedNumber}</span>
-              <span>
-                Period: {room.periodStart} - {room.periodEnd}
-              </span>
-              <span>Price: {room.price}</span>
-              {room.roomFeatures && (
-                <div className="roomFeatures">
-                  <h4>Room Features:</h4>
-                  {room.roomFeatures.map((roomFeature, index) => (
-                    <div key={index} className="roomFeature">
-                      <span>{roomFeature.name}</span>
-                    </div>
+        <ClickAwayListener onClickAway={() => setUpdateRoom({})}>
+          <TableContainer component={Paper}>
+            <Table aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  {dataRoom.map((data) => (
+                    <TableCell key={data.dbName}>{data.siteName}</TableCell>
                   ))}
-                </div>
-              )}
-              <span>Hotel Name: {room.hotelName}</span>
-              <span onClick={handleDeleteButton} id={room.id}>
-                <FontAwesomeIcon icon={faTrash} className="delete-icon" />
-              </span>
-            </div>
-          </div>
-        ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {roomQuery.data?.data?.data.map((room) => (
+                  <TableRow
+                    key={room.id}
+                    onClick={() => handleUpdateBtn(room.id)}
+                    id={room.id}
+                  >
+                    {dataRoom.map((data) => (
+                      <TableCell key={`tablecell${data.dbName}`}>
+                        {updateRoom.id === room.id && data.type === "text" ? (
+                          <TextField
+                            id="outlined-basic"
+                            variant="standard"
+                            name={data.dbName}
+                            className={`${
+                              updateRoom.id === room.id ? "update" : ""
+                            }`}
+                            value={updateRoom[data.dbName]}
+                            onChange={(event) => {
+                              setUpdateRoom({
+                                ...updateRoom,
+                                [data.dbName]: event.target.value,
+                              });
+                            }}
+                          />
+                        ) : updateRoom.id === room.id &&
+                          data.type === "select" ? (
+                          <select
+                            name={data.dbName}
+                            required
+                            value={updateRoom[data.dbName]?.id || ""}
+                            onChange={(e) =>
+                              setUpdateRoom({
+                                ...updateRoom,
+                                [data.dbName]: { id: e.target.value },
+                              })
+                            }
+                          >
+                            <option value="" disabled>
+                              {data.selectText}
+                            </option>
+                            {data.options === "roomTypeQuery"
+                              ? roomTypeQuery.data?.data?.data.map(
+                                  (roomType) => (
+                                    <option
+                                      key={roomType.id}
+                                      value={roomType.id}
+                                    >
+                                      {roomType.name}
+                                    </option>
+                                  )
+                                )
+                              : data.options === "hotelQuery"
+                              ? hotelQuery.data?.data?.data.map((hotel) => (
+                                  <option key={hotel.id} value={hotel.id}>
+                                    {hotel.name}
+                                  </option>
+                                ))
+                              : "error"}
+                          </select>
+                        ) : updateRoom.id === room.id &&
+                          data.type === "checkbox" ? (
+                          roomFeatureQuery.data?.data?.data.map(
+                            (roomFeature) => (
+                              <div key={roomFeature.id}>
+                                <input
+                                  type="checkbox"
+                                  checked={updateRoom.roomFeatures.find(
+                                    (roomF) => roomF.id === roomFeature.id
+                                  )}
+                                  name="facilities"
+                                  value={roomFeature.id}
+                                  onChange={(event) => {
+                                    if (event.target.checked) {
+                                      setUpdateRoom({
+                                        ...updateRoom,
+                                        roomFeatures: [
+                                          ...updateRoom.roomFeatures,
+                                          { id: event.target.value },
+                                        ],
+                                      });
+                                    } else {
+                                      setUpdateRoom({
+                                        ...updateRoom,
+                                        roomFeatures:
+                                          updateRoom.roomFeatures.filter(
+                                            (facility) =>
+                                              facility.id !== event.target.value
+                                          ),
+                                      });
+                                    }
+                                  }}
+                                />
+                                <label>{roomFeature.name}</label>
+                              </div>
+                            )
+                          )
+                        ) : data.dbName === "roomFeatures.name" ? (
+                          room.roomFeatures.map((roomFeature) => (
+                            <span key={roomFeature.id}>{roomFeature.name}</span>
+                          ))
+                        ) : data.dbName === "roomType.name" ? (
+                          room.roomType.name
+                        ) : (
+                          room[data.dbName]
+                        )}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </ClickAwayListener>
       </div>
       <div className="secondary-container">
         <div className="room-types-container">
